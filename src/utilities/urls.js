be83bc687b334a193ef4cc7bf9d91202af64785e
ManikanthDindigala <dindigala@sub.uni-goettingen.de>
2022-02-18 10:04:25 +0100

    feat: placing logos at the bottom of the page.

---
+++ src/utilities/urls.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    image: 'logo-uni-goettingen.svg',
+    title: 'Georg August University Goettingen',
+    to: 'https://www.gwdg.de/',
+  },
+  {
+    image: 'SUB_left_horizontal_white.svg',
+    title: 'State- and University Library Goettingen',
+    to: 'https://www.sub.uni-goettingen.de/sub-aktuell/',
+  },
+  {
+    image: 'dfg_logo_schriftzug_weiss.svg',
+    title: 'Deutsche Forschungsgemeinschaft e.V.',
+    to: 'https://www.dfg.de/',
+  },
+];
