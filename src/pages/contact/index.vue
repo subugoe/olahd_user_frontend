110947a4795077686baf5e3586f1704b83ef8360
ManikanthDindigala <dindigala@sub.uni-goettingen.de>
2022-07-06 10:06:57 +0200

    feat: replace google maps with SUB open maps.

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -35,7 +35,7 @@
             37073 Göttingen
           </p>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2481.5679692807053!2d9.934229452490921!3d51.53948349233777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a4d4c1219da0a5%3A0xace1444b395f347b!2sG%C3%B6ttingen%20State%20and%20University%20Library!5e0!3m2!1sen!2sde!4v1595012969765!5m2!1sen!2sde"
+            src="https://www.geodata.uni-goettingen.de/Lageplan_mobile/?zoom=1&piz=5383&etage=EG"
             width="100%"
             height="450"
             frameborder="0"
@@ -53,7 +53,7 @@
             37077 Göttingen
           </p>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2480.3946194240125!2d9.96558541337793!3d51.5609989219582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x230e28ac4054dc44!2sGesellschaft%20f%C3%BCr%20wissenschaftliche%20Datenverarbeitung%20mbH%20G%C3%B6ttingen!5e0!3m2!1sen!2sde!4v1595012909464!5m2!1sen!2sde"
+            src="https://www.geodata.uni-goettingen.de/Lageplan_mobile/?zoom=1&piz=0775&etage=EG"
             width="100%"
             height="450"
             frameborder="0"
