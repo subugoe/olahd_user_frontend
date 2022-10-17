91b5a146abeefd7b49c9f307c9b27516f10da607
Yasin Onur Yücel <yuecel@sub.uni-goettingen.de>
2022-10-17 19:39:36 +0200

    Add item fields from result set

--- src/components/search/SearchResult.vue
+++ src/components/search/SearchResult.vue
@@ -14,12 +14,32 @@
     <hr class="mt-3 mb-2" />
     <div class="space-y-2">
       <div class="flex items-center flex-1">
+        <h5 class="text-sm text-gray-700 w-36">Subtitle</h5>
+        <p class="flex-1 text-sm">{{ subtitle }}</p>
+      </div>
+      <div class="flex items-center flex-1">
+        <h5 class="text-sm text-gray-700 w-36">Publisher</h5>
+        <p class="flex-1 text-sm">{{ publisher }}</p>
+      </div>
+      <div class="flex items-center flex-1">
         <h5 class="text-sm text-gray-700 w-36">Creator</h5>
         <p class="flex-1 text-sm">{{ creator }}</p>
       </div>
       <div class="flex items-center flex-1">
-        <h5 class="text-sm text-gray-700 w-36">Published year</h5>
+        <h5 class="text-sm text-gray-700 w-36">Published Place</h5>
+        <p class="flex-1 text-sm">{{ placeOfPublish }}</p>
+      </div>
+      <div class="flex items-center flex-1">
+        <h5 class="text-sm text-gray-700 w-36">Published Year</h5>
         <p class="flex-1 text-sm">{{ yearPublish }}</p>
+      </div>
+      <div class="flex items-center flex-1">
+        <h5 class="text-sm text-gray-700 w-36">PID</h5>
+        <p class="flex-1 text-sm">{{ itemId }}</p>
+      </div>
+      <div class="flex items-center flex-1">
+        <h5 class="text-sm text-gray-700 w-36">Ground Truth</h5>
+        <p class="flex-1 text-sm">{{ isGT }}</p>
       </div>
     </div>
   </div>
@@ -49,11 +69,23 @@ export default {
     itemId() {
       return this.item.PID;
     },
+    publisher() {
+      return this.item.publisher || "N/A";
+    },
     creator() {
       return this.item.creator || "N/A";
     },
     yearPublish() {
       return this.item.yearOfPublish || "N/A";
+    },
+    placeOfPublish() {
+      return this.item.placeOfPublish || "N/A";
+    },
+    subtitle() {
+      return this.item.subtitle || "N/A";
+    },
+    isGT() {
+      return this.item.isGT || "N/A";
     },
     dateCreated() {
       return this.item.reindexedat;
