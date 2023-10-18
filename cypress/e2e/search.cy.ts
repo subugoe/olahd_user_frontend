c7ed266ddb1d168dc09e23101aca8f2fe475c8e1
joschrew <jonas.schrewe@gwdg.de>
2023-10-18 16:54:50 +0200

    Make cypress tests run again

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,24 +1,24 @@
 describe("Search", () => {
   it("Should display search header on home page", () => {
-    cy.visit('/home').then(() => {
+    cy.visit('/').then(() => {
       cy.get("#search-header").should("be.visible");
     });
   });
 
   it("Should not visible search header on other pages", () => {
-    cy.visit('/home/about').then(() => {
+    cy.visit('/about').then(() => {
       cy.get("#search-header").should("not.exist");
     });
   });
 
   it("Should display small search header on other pages", () => {
-    cy.visit("/home/about").then(() => {
+    cy.visit("/about").then(() => {
       cy.get("#search-header-small").should("be.visible");
     });
   });
 
   it("Should redirect to correct page after performing query",()=>{
-    cy.visit('/home').then(() => {
+    cy.visit('/').then(() => {
       cy.get("#search-header").should("be.visible");
       cy.get('#search-box').type('berlin',{force:true});
       cy.get("#search-header button[type='submit']").click();
@@ -27,7 +27,7 @@ describe("Search", () => {
   })
 
   it("Should redirect to correct page after performing query on small search header", () => {
-    cy.visit('/home/about').then(() => {
+    cy.visit('/about').then(() => {
       cy.get("#search-header-small").should("be.visible");
       cy.get('#search-header-small input').type('berlin');
       cy.get("#search-header-small button[type='submit']").click();
@@ -37,16 +37,16 @@ describe("Search", () => {
 
 
   it("Should display search results correctly", () => {
-    cy.visit('/home/search?q=berlin').then(() => {
+    cy.visit('/search?q=berlin').then(() => {
       cy.get('.search-item').should('have.length.at.least', 1);
 
       cy.get('.search-item-link').first().click({force:true});
-      cy.url().should('include','/home/search-detail')
+      cy.url().should('include','/search-detail')
     })
   })
-  
+
    it("Should not display search results when incorrect search happens", () => {
-    cy.visit('/home/search?q=olahd').then(() => {
+    cy.visit('/search?q=olahd').then(() => {
       cy.get('.search-item').should('have.length', 0);
     })
    })
