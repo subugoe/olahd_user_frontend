4de6cc44a1a2b8ae74dd14c0d36cd094432f6738
joschrew <jonas.schrewe@gwdg.de>
2023-12-06 12:03:24 +0100

    Adapt router's beforeEach to vue3
    In vue 3 in the beforeEach function of the router the next function
    must be called exactly once. Async calls to the auth service are
    awaited because of that and the if-statement is changed to always call
    next once in every branch.

--- src/router/index.ts
+++ src/router/index.ts
@@ -74,33 +74,31 @@ router.beforeEach(async (to, from, next) => {
   //   An additional file would lead to a short hick-up after logging in. (callback.html is
   //   loaded and than the actual route.)
   // So here we handle the login redirect and than send the user to the "/" route.
-  if (authService.isKeycloak()) {
+  if (authService.isKeycloak() && (to.path === '/login' || to.path === '/logout')) {
     if (to.path === '/login') {
       if (to.fullPath.includes("error")) {
         return;
       }
       // Inform the authentication of the login redirect. Afterwards we send the user to the
       // destination page
-      authService.handleLoginRedirect()
-        .then(() => {
-          next('/dashview/dashboard')
-          // this is indirectly used to propagate the login status to its listeners
-          authService.isUserLoggedIn()
-        })
-        .catch((error: object) => {
-          next('/')
-        })
+      try {
+        await authService.handleLoginRedirect();
+        next('/dashview/dashboard');
+      } catch (error) {
+        next('/');
+      }
+      // this is indirectly used to propagate the login status to its listeners      
+      authService.isUserLoggedIn()
     } else if (to.path === '/logout') {
       // This is similar to the "/callback" route not leading to an actual component but only to handle the logout callback from the authentication server.
-      authService.handleLogoutRedirect()
-        .then(() => {
-          next('/')
-        })
+      await authService.handleLogoutRedirect()
+      next('/');
+      console.log("logout-redirect handle next");
     }
+  } else {
+    // Default case. The user is send to the desired route.
+    next();
   }
-
-  // Default case. The user is send to the desired route.
-  next()
 })
 
 export default router
