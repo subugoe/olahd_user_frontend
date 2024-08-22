8bf827439b71367c3f96c87c39f1c154c0cedaef
joschrew <91774427+joschrew@users.noreply.github.com>
2024-08-22 09:21:37 +0200

    Do not keep scroll position after page change

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,10 @@ import { authService } from '../auth/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
   routes: [
     {
       path: "/",
