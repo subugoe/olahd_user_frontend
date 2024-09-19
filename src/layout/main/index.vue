5a300a2d2f6fd681c5a48d133c0ece55b7adc793
joschrew <91774427+joschrew@users.noreply.github.com>
2024-09-19 14:37:51 +0200

    Replace search-select-button on home page
    The button was problematic on mobile screens and now it is even more
    obvious that there are search options.

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -6,47 +6,43 @@
     <h4 class="flex justify-center m-5 text-3xl text-gray-600">
       OLA-HD - OCR Long-Term Archive
     </h4>
-    <div class="col-md-6 col-sm-8 mx-auto">
-      <form @submit.prevent="submit" class="grid grid-cols-7">
-        <label class="relative block col-start-3 col-span-3">
-          <input
-            class="
-              w-full
-              border-3 border-sky-500
-              drop-shadow-md
-              rounded-full
-              py-2
-              pl-3
-              pr-10
-            "
-            id="search-box"
-            type="text"
-            placeholder="Search..."
-            v-model="query"
-          />
-          <span class="absolute inset-y-0 right-0 flex items-center">
-            <button class="btn btn-link mr-4" type="submit">
-              <i class="fas fa-search fa-lg text-sky-500"></i>
-            </button>
-            <MultiSelect
-              id="dropdownCheckboxButton"
-              v-model="selectedFilters"
-              :options="[
-                {name: 'Metadata Search', code: 'meta'},
-                {name: 'Full Text Search', code: 'ft'},
-                {name: 'GT-Search', code: 'gt'},
-              ]"
-              optionLabel="name"
-              :maxSelectedLabels="0"
-              :showToggleAll="false"
-              :selectionLimit="null"
-              :highlightOnSelect="true"
-              selectedItemsLabel="Select"
-              placeholder="Select"
-              class="bg-sky-700 rounded-full w-34 rounded-l-lg px-2 h-10 text-green-200 border-r "
+    <div class="mx-auto">
+      <form @submit.prevent="submit" class="float">
+        <div class="grid grid-cols-7">
+          <label class="relative block col-start-1 col-span-7 sm:col-start-2 sm:col-span-5 md:col-start-3 md:col-span-3">
+            <input
+              class="
+                w-full
+                border-3 border-sky-500
+                drop-shadow-md
+                rounded-full
+                py-2
+                pl-3
+                pr-10
+              "
+              id="search-box"
+              type="text"
+              placeholder="Search..."
+              v-model="query"
             />
-          </span>
-        </label>
+            <span class="absolute inset-y-0 right-0 flex items-center">
+              <button class="btn btn-link mr-4" type="submit">
+                <i class="fas fa-search fa-lg text-sky-500"></i>
+              </button>
+            </span>
+          </label>
+        </div>
+        <div class="grid grid-cols-2 divide-x divide-black mt-2">
+          <div class="justify-self-end m-1">Search in</div>
+          <div class="grid grid-cols-2 m-1" style="grid-template-columns: auto 1fr;">
+            <input type="checkbox" v-model="metadatasearch" id="check-meta" class="m-1 bg-yellow-300">
+            <label for="check-meta">Metadata</label>
+            <input type="checkbox" v-model="fulltextsearch" id="check-fulltext" class="m-1">
+            <label for="check-fulltext">Full-Text</label>
+            <input type="checkbox" v-model="gtsearch" id="check-gt" class="m-1">
+            <label for="check-gt">Ground Truth</label>
+          </div>
+        </div>
       </form>
     </div>
   </div>
@@ -62,15 +58,12 @@ export default {
     return {
       query: this.$route.query["q"],
       filter: "",
-      selectedFilters: [
-        {name: 'Metadata Search', code: 'meta'}
-      ],
+      metadatasearch: "true",
+      fulltextsearch: "false",
+      gtsearch: "false",
     };
   },
   methods: {
-    isFilterSelected(name) {
-      return this[name] === "true";
-    },
     submit() {
       // Trim the query
       this.query = (this.query || "").trim();
@@ -80,9 +73,9 @@ export default {
           name: "search",
           query: {
             q: this.query,
-            fulltextsearch: this.selectedFilters.some(element => element.code === 'ft'),
-            metadatasearch: this.selectedFilters.some(element => element.code === 'meta'),
-            isGT:this.selectedFilters.some(element => element.code === 'gt'),
+            fulltextsearch: this.fulltextsearch,
+            metadatasearch: this.metadatasearch,
+            isGT: this.gtsearch,
           },
         })
         .catch(() => {}); // To ignore the Navigation Duplicated error
