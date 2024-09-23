dba981325a2f332d55fb9d18a4e4493aa6a0008f
joschrew <91774427+joschrew@users.noreply.github.com>
2024-09-23 14:55:12 +0200

    Center home page text and change margin between p

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="m-4 space-y-4">
-    <h3 class="text-3xl">Welcome</h3>
-    <p>
-      OLA-HD provides a service for the long-term archiving of historical prints in the context of
-      <a class="text-sky-500 hover:text-sky-700" href="https://ocr-d.de/en"
-        >OCR-D</a
-      >.
-      Archive objects can be found, viewed and downloaded. We welcome feedback on the beta version
-      of the service.
-    </p>
-    <br />
-    <p>
-      OLA-HD is funded by the DFG initiative
-      <a class="text-sky-500 hover:text-sky-700" href="https://ocr-d.de/en"
-        >OCR-D</a
-      >.
-      The main objective of OCR-D is the conceptual and technical preparation of the full-text
-      transformation of printed works published in the German-speaking world between the 16th and
-      18th centuries. The task of automatic full-text recognition is broken down into its individual
-      process steps, which can be reproduced in the open source OCR-D software. This makes it
-      possible to create optimal workflows and generate scientifically usable full texts. OLA-HD
-      offers a service for the long-term archiving of these results.
-    </p>
+  <div class="flex justify-center">
+    <div class="m-6 w-3/4 2xl:w-1/2">
+      <h3 class="text-3xl mb-6">Welcome</h3>
+      <p class="mb-12">
+        OLA-HD provides a service for the long-term archiving of historical prints in the context of
+        <a class="text-sky-500 hover:text-sky-700" href="https://ocr-d.de/en"
+          >OCR-D</a
+        >.
+        Archive objects can be found, viewed and downloaded. We welcome feedback on the beta version
+        of the service.
+      </p>
+      <p>
+        OLA-HD is funded by the DFG initiative
+        <a class="text-sky-500 hover:text-sky-700" href="https://ocr-d.de/en"
+          >OCR-D</a
+        >.
+        The main objective of OCR-D is the conceptual and technical preparation of the full-text
+        transformation of printed works published in the German-speaking world between the 16th and
+        18th centuries. The task of automatic full-text recognition is broken down into its individual
+        process steps, which can be reproduced in the open source OCR-D software. This makes it
+        possible to create optimal workflows and generate scientifically usable full texts. OLA-HD
+        offers a service for the long-term archiving of these results.
+      </p>
+    </div>
   </div>
 </template>
 
