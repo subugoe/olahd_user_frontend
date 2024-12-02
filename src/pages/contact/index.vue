33ad30cf3836c8e97afd8b0dd9059960ab7e0a8a
joschrew <91774427+joschrew@users.noreply.github.com>
2024-12-02 11:19:02 +0100

    Revise contact page

--- src/pages/contact/index.vue
+++ src/pages/contact/index.vue
@@ -1,79 +1,21 @@
 <template>
-  <section id="contact">
-    <div class="container">
-      <div class="row mt-3">
-        <div class="col">
-          <h2>Contact</h2>
-          <hr />
-          <h4>General requests</h4>
-          <p>
-            Lilja Sautter:
-            <a href="mailto:sautter@sub.uni-goettingen.de">
-              sautter@sub.uni-goettingen.de
-            </a>
-          </p>
-          <!--<a
-            href="https://github.com/subugoe/OLA-HD-IMPL/issues"
-            target="_blank"
-          >
-            Report error
-          </a>-->
-        </div>
+  <section id="contact" class="grid mx-2 sm:mt-10 justify-center">
+      <h1 class="mb-4 text-4xl font-bold">Contact</h1>
+      <div class="text-left">
+        <h2 class="text-xl font-bold">General requests</h2>
+        <p>
+          Lilja Sautter:
+          <a href="mailto:sautter@sub.uni-goettingen.de" class="olahd-link-color">
+            sautter@sub.uni-goettingen.de
+          </a>
+        </p>
+        <h2>Address</h2>
+        <p class="mb-3">
+          Niedersächsische Staats- und Universitätsbibliothek<br />
+          Platz der Göttinger Sieben 1<br />
+          37073 Göttingen
+        </p>
       </div>
-      <hr />
-      <div class="row">
-        <div class="col">
-          <h4>Address</h4>
-        </div>
-      </div>
-      <div class="row mb-2">
-        <div class="col-6">
-          <p class="mb-3">
-            Niedersächsische Staats- und Universitätsbibliothek<br />
-            Platz der Göttinger Sieben 1<br />
-            37073 Göttingen
-          </p>
-          <small
-            ><a
-              target="_blank"
-              href="https://www.openstreetmap.org/#map=18/51.53948/9.93525"
-              >View Larger Map</a
-            ></small
-          >
-          <iframe
-            width="100%"
-            height="350"
-            frameborder="0"
-            scrolling="no"
-            src="https://www.openstreetmap.org/export/embed.html?bbox=9.933303594589235%2C51.53837457803422%2C9.937203526496889%2C51.54059003724594&amp;layer=mapnik"
-            style="border: 1px solid black"
-          ></iframe>
-        </div>
-        <div class="col-6">
-          <p class="mb-3">
-            Gesellschaft für wissenschaftliche Datenverarbeitung mbH
-            Göttingen<br />
-            Burckhardtweg 4<br />
-            37077 Göttingen
-          </p>
-          <small>
-            <a
-              target="_blank"
-              href="https://www.openstreetmap.org/#map=19/51.56100/9.95702"
-              >View Larger Map</a
-            >
-          </small>
-          <iframe
-            width="100%"
-            height="350"
-            frameborder="0"
-            scrolling="no"
-            src="https://www.openstreetmap.org/export/embed.html?bbox=9.955710768699648%2C51.56044761236335%2C9.957660734653475%2C51.561554818121394&amp;layer=mapnik"
-            style="border: 1px solid black"
-          ></iframe>
-        </div>
-      </div>
-    </div>
   </section>
 </template>
 
@@ -84,4 +26,10 @@ export default {
 </script>
 
 <style scoped>
+  h1 {
+    @apply mb-4 text-4xl font-semibold text-slate-700
+  }
+  h2 {
+    @apply text-xl font-semibold mt-10 mb-2 text-slate-700
+  }
 </style>
