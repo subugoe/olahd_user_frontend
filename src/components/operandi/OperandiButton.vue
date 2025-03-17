37d6b9623b330a83749a4cea0544aacad922262b
joschrew <91774427+joschrew@users.noreply.github.com>
2025-03-17 14:15:38 +0100

    Add a confirm dialog before starting operandi

--- src/components/operandi/OperandiButton.vue
+++ src/components/operandi/OperandiButton.vue
@@ -1,6 +1,32 @@
 <template>
+  <Dialog
+    header="Start Operandi-Workflow"
+    :append-to-body="true"
+    v-model:visible="showDialog"
+    modal
+    dismissableMask
+    @hide=""
+    :pt="{
+      title: {
+        class: ['text-lg', 'font-medium'],
+      },
+    }"
+    :class="{ 'w-1/4' : !isMobile }"
+  >
+    <div>
+      <p class="mb-4">The workspace is transferred to operandi.ocr-d.de and then a workflow is started to process
+         the workspace. Once the workflow has finished, the processed workspace is uploaded back to OLA-HD and
+         saved as a next version. 
+         Depending on the complexity of the workspace to be processed, this can take several hours
+         until the job is completed</p>
+      <div class="flex place-content-center">
+        <button class="detail-blue-button flex-none" @click="runOperandi">Run</button>
+        <button class="detail-blue-button flex-none" @click="this.showDialog = false">Cancel</button>
+      </div>
+    </div>
+  </Dialog>
   <div>
-    <button :disabled="disable" @click="runOperandi" class="w-fit detail-blue-button"
+    <button :disabled="disable" @click="operandiButtonClicked" class="w-fit detail-blue-button"
       v-tooltip="tooltip"
       v-show="show">
       <i class="fas fa-newspaper mr-1" />
@@ -13,8 +39,10 @@
 
 <script>
 import lzaApi from "@/services/lzaApi";
+import Dialog from "primevue/dialog";
 
 export default {
+  components: { Dialog },
   props: {
     pid: {
       type: String,
@@ -35,6 +63,7 @@ export default {
       jobId: null,
       disable: true,
       errorMsg: "",
+      showDialog: false,
     };
   },
   computed: {
@@ -44,6 +73,10 @@ export default {
     }
   },
   methods: {
+    operandiButtonClicked() {
+      this.showDialog = true
+    },
+
     runOperandi() {
       //double check logged in user not neseccary, as backend call is authenticated
       this.disable = true;
@@ -57,6 +90,7 @@ export default {
           this.disable = true;
           this.tooltip = "Starting failed"
         });
+      this.showDialog = false;
     },
 
     async enableOperandiButton() {
