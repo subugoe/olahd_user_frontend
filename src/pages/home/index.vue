837a9c30864180c6459fdd6782233a60cf87efd6
joschrew <91774427+joschrew@users.noreply.github.com>
2025-08-11 09:19:27 +0200

    Change welcome text

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,23 +4,15 @@
       <h3 class="text-3xl mb-6">Welcome</h3>
       <p class="mb-12">
         OLA-HD provides a service for the long-term archiving of historical prints in the context of
-        <a class="olahd-link-color olahd-link-underline" href="https://ocr-d.de/en"
-          >OCR-D</a
-        >.
-        Archive objects can be found, viewed and downloaded. We welcome feedback on the beta version
-        of the service.
+        <a class="olahd-link-color olahd-link-underline" href="https://ocr-d.de/en">OCR-D</a>.
+        Archive objects can be imported, found, viewed and downloaded.
       </p>
       <p>
-        OLA-HD is funded by the DFG initiative
-        <a class="olahd-link-color olahd-link-underline" href="https://ocr-d.de/en"
-          >OCR-D</a
-        >.
-        The main objective of OCR-D is the conceptual and technical preparation of the full-text
+        OLA-HD was funded by the DFG initiative
+        <a class="olahd-link-color olahd-link-underline" href="https://ocr-d.de/en">OCR-D</a>.
+        The main objective of OCR-D was the conceptual and technical preparation of the full-text
         transformation of printed works published in the German-speaking world between the 16th and
-        18th centuries. The task of automatic full-text recognition is broken down into its individual
-        process steps, which can be reproduced in the open source OCR-D software. This makes it
-        possible to create optimal workflows and generate scientifically usable full texts. OLA-HD
-        offers a service for the long-term archiving of these results.
+        18th centuries.
       </p>
     </div>
   </div>
