e9bc30a87885684745c83271eed2f4757e8d32a0
joschrew <91774427+joschrew@users.noreply.github.com>
2026-02-16 14:45:11 +0100

    Upgrade pinia, set sidbar extended by default

--- src/stores/settings.js
+++ src/stores/settings.js
@@ -3,7 +3,7 @@ import { ref } from 'vue'
 
 export const useSettingsStore = defineStore('settings', () => {
   const isMobile = ref(false)
-  const isSidebarExpanded = ref(false)
+  const isSidebarExpanded = ref(true)
 
   function setIsMobile(newIsMobile) {
     isMobile.value = newIsMobile
